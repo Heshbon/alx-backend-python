2610730a39bf1880c187bf6bb809611ec5289316
Heshbon <cityalight.hesbon@gmail.com>
2024-08-01 22:03:21 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module type-annotated function that defines add function."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    arguments:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    return a+b
