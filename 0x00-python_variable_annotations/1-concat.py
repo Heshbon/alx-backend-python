ddf6c89c13d8b42eca3733471f4eaf67f21a6fe5
Heshbon <cityalight.hesbon@gmail.com>
2024-08-01 22:07:45 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module that concatenate two strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    arguments:
+    str1 (str): First string.
+    str2 (str): Second string.
+
+    Returns:
+    The concatenated string.
+    """
+    return str1 + str2
