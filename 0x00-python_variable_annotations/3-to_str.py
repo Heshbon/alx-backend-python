890541ba43257b6fb0290879cf909b01ca0e8ed3
Heshbon <cityalight.hesbon@gmail.com>
2024-08-01 22:30:29 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module function that converts a float to a strng."""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float number to its string representation.
+
+    argument:
+    n (float): The float number to be converted.
+
+    Returns:
+    The string representation of the float.
+    """
+    return str(n)
