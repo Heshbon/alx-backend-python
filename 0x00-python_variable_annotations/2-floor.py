f3efd68b90373303c1478b459333c679dddb767b
Heshbon <cityalight.hesbon@gmail.com>
2024-08-01 22:13:57 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Module parameter that solves a float argument."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float number.
+
+    arguments:
+    n (float): The float number to find the floor of.
+
+    Returns:
+    The floor of the float.
+    """
+    return math.floor(n)
